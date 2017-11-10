a5537028d00bb987f50f5f03670f1aa923846ef8
y.fomichev <yury.fomichov@gmail.com>
2017-11-10 15:27:57 +0300

    update model

--- src/model.py
+++ src/model.py
@@ -37,5 +37,5 @@ class ShipModel(nn.Module):
             p.requires_grad = False
 
     def densenet63(self, **kwargs):
-        return models.DenseNet(num_init_features=64, growth_rate=32, block_config=(3, 6, 12, 8), drop_rate=0.3, **kwargs)
+        return models.DenseNet(num_init_features=64, growth_rate=32, block_config=(3, 6, 10, 8), drop_rate=0.2, **kwargs)
 
