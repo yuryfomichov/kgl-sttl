0f28fe270f2556b2cab706a04024acc6f69525cb
y.fomichev <yury.fomichov@gmail.com>
2017-11-10 13:45:57 +0300

    update model

--- src/dataset.py
+++ src/dataset.py
@@ -37,7 +37,6 @@ class ShipsDataset(data.Dataset):
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
             transforms.RandomRotation(0.2),
-            transforms.ColorJitter(0.5, 0.5, 0.5, 0),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])])
