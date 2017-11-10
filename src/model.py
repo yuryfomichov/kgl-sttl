4085b6c9da622305c9c7cdda866be07f9ff2cade
y.fomichev <yury.fomichov@gmail.com>
2017-11-10 14:29:17 +0300

    update model

--- src/model.py
+++ src/model.py
@@ -6,36 +6,7 @@ class ShipModel(nn.Module):
     def __init__(self, num_classes=2):
         super(ShipModel, self).__init__()
 
-        self.model = models.resnet18(pretrained= False, num_classes = num_classes)
-        # self.features = nn.Sequential(
-        #     nn.Conv2d(3, 128, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(128),
-        #     nn.ReLU(inplace=True),
-        #     nn.MaxPool2d(kernel_size=2, stride=2),
-        #     nn.Conv2d(128, 192, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(192),
-        #     nn.ReLU(inplace=True),
-        #     nn.MaxPool2d(kernel_size=2, stride=2),
-        #     nn.Conv2d(192, 256, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(256),
-        #     nn.ReLU(inplace=True),
-        #     nn.MaxPool2d(kernel_size=2, stride=2),
-        #     nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-        #     nn.BatchNorm2d(384),
-        #     nn.ReLU(inplace=True),
-        #     nn.AvgPool2d(kernel_size=8, stride=2),
-        # )
-        # # self._require_grad_false()
-        #
-        # self.classifier = nn.Sequential(
-        #     nn.Linear(384, 1024),
-        #     nn.BatchNorm1d(1024),
-        #     nn.ReLU(True),
-        #     nn.Linear(1024, 1024),
-        #     nn.BatchNorm1d(1024),
-        #     nn.ReLU(True),
-        #     nn.Linear(1024, num_classes),
-        # )
+        self.model = self.densenet35(pretrained= False, num_classes = num_classes)
         self._initialize_weights()
 
     def forward(self, x):
@@ -64,3 +35,7 @@ class ShipModel(nn.Module):
     def _require_grad_false(self):
         for p in self.features.parameters():
             p.requires_grad = False
+
+    def densenet35(pretrained=False, **kwargs):
+        return models.DenseNet(num_init_features=64, growth_rate=32, block_config=(2, 3, 6, 4), **kwargs)
+
